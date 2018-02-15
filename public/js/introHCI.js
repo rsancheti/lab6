23583e59972fb835262a641f68870a8c56a096c1
Rajiv Sancheti <rsanchet@ucsd.edu>
2018-02-15 10:54:37 -0800

    added all changes

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -8,23 +8,36 @@ $(document).ready(function() {
 /*
  * Function that is called when the document is ready.
  */
-function initializePage() {
+function initializePage(event) {
 	$('.project a').click(addProjectDetails);
-
-	$('#colorBtn').click(randomizeColors);
+	//$('#colorBtn').click(randomizeColors);
 }
 
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
-function addProjectDetails(e) {
+function addProjectDetails(event) {
 	// Prevent following the link
-	e.preventDefault();
+	event.preventDefault();
 
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
-	// get rid of 'project' from the front of the id 'project3'
+
+		// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("http://localhost:3000/project/" + idNumber, callBackFn)
+
 	console.log("User clicked on project " + idNumber);
+}
+
+function callBackFn(result) {
+	console.log(result)
+  var projectHTML = '<a href="#" class="thumbnail">' +
+    '<img src="' + result['image'] + '" class="detailsImage">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+    '</small></p></a>' + '<p>' + result['summary'] + '</p>'; 
+
+		$("#project" + result.id + " .details").html(projectHTML);
 }
